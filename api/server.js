a2d738ea20e52b46be5d9e8fc56eebd5b3b7b9ee
Dionne <mylife33@hotmail.com>
2025-10-11 19:33:56 -0500

    removed left over unused auth routing

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 
 const songRouter = require("./routes/songRoutes");
-const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const server = express();
@@ -11,7 +10,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/songs", songRouter);
-server.use("/auth", authRouter);
 server.use("/users", userRouter);
 
 server.use((err, req, res, next) => {
