cc8a73c07f523ef473f0979f9a5b1458c1654a1e
Dionne <mylife33@hotmail.com>
2025-10-16 13:08:52 -0500

    added new playlist background image and color keys

--- api/models/Playlist.js
+++ api/models/Playlist.js
@@ -5,6 +5,8 @@ const playlistSchema = new mongoose.Schema(
     name: { type: String, required: true },
     owner: { type: String, required: true }, // stores Auth0 ID instead of ObjectId
     songs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Song" }],
+    background_image: { type: String, default: "" },
+    details_color: { type: String, default: "#6e6c6cff" },
   },
   { timestamps: true }
 );
