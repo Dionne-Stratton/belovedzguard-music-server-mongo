e02f2b2b3b1da08b766dfa4470eecdc88ad5137e
Dionne <mylife33@hotmail.com>
2025-10-16 17:28:55 -0500

    updated theme in playlist schema

--- api/models/Playlist.js
+++ api/models/Playlist.js
@@ -5,8 +5,7 @@ const playlistSchema = new mongoose.Schema(
     name: { type: String, required: true },
     owner: { type: String, required: true }, // stores Auth0 ID instead of ObjectId
     songs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Song" }],
-    background_image: { type: String, default: "" },
-    details_color: { type: String, default: "#6e6c6cff" },
+    theme: { type: String, default: "faith" },
   },
   { timestamps: true }
 );
